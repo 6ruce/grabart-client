49cacd80f5f29abca969b0c3595a2c13f62aab9d
bruce <i6ruce@gmail.com>
2012-08-29 01:37:26 +0300

    Building script deployer.

--- GrabArtDeploy/Program.cs
+++ GrabArtDeploy/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
 
 namespace GrabArtDeploy
 {
@@ -10,6 +10,48 @@ namespace GrabArtDeploy
     {
         static void Main(string[] args)
         {
+            new FileOperator("../entry.js").WriteAll(new Builder().Build(new FileOperator("main.js").ReadAll()));
+            Console.ReadKey();
+        }
+    }
+
+    class FileOperator 
+    {
+        const string PRE_PATH = "../../JS/";
+
+        private string fileName;
+
+        public FileOperator(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public string ReadAll() 
+        {
+            Console.WriteLine("->File `" + fileName + "` opened for reading.");
+            return File.ReadAllText(PRE_PATH + fileName);
+        }
+
+        public void WriteAll(string text)
+        {
+            Console.WriteLine("->File `" + fileName + "` opened for writing.");
+            File.WriteAllText(PRE_PATH + fileName, text);
+        }
+    }
+
+    class Builder 
+    {
+        public string Build(string content)
+        {
+            Match match = Regex.Match(content, @"\('\[(.+)\]'\)");
+            Console.Write("Found matches: ");
+            for (int i = 1; i < match.Groups.Count; i++) {
+                string module = match.Groups[i].Value;
+                Console.Write(module + " ");
+                Build(new FileOperator(module.Replace('.','/') + ".js").ReadAll());
+            }
+            Console.WriteLine();
+            return content;
         }
     }
 }
